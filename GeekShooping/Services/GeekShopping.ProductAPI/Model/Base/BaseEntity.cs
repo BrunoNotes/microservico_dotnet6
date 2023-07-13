0c32b986dfbc4600956251b463336f493e8c86b3
BrunoNotes <brunonotes7@gmail.com>
2023-07-13 00:30:27 -0300

    Finalizado Migrations
    Finalizado migrations

---
+++ GeekShooping/Services/GeekShopping.ProductAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model.Base {
+    public class BaseEntity {
+        [Key]
+        [Column("id")]
+        public long id { get; set; }
+    }
+}
